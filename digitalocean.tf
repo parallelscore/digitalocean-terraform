b21defd644b07cc7896802d967af6fdc8632aef5
Nnolum Esther <esthernnolum@gmail.com>
2022-05-31 11:42:52 +0100

    Update digitalocean.tf

--- digitalocean.tf
+++ digitalocean.tf
@@ -1,36 +1,33 @@
 terraform {
 
-# terraform {
-#       backend "remote" {
-#         # The name of your Terraform Cloud organization.
-#         organization = "example-organization"
-#
-#         # The name of the Terraform Cloud workspace to store Terraform state files in.
-#         workspaces {
-#           name = "example-workspace"
-#         }
-#       }
-#     }
-#
-#     # An example resource that does nothing.
-#     resource "null_resource" "example" {
-#       triggers = {
-#         value = "A example resource that does nothing!"
-#       }
-#     }
+terraform {
+  backend "remote" {
+        # The name of your Terraform Cloud organization.
+         organization = "example-organization"
+
+         # The name of the Terraform Cloud workspace to store Terraform state files in.
+         workspaces {
+           terraform.workspace= networking-prod
+         }
+       }
+     }
+
+     # An example resource that does nothing.
+     resource "null_resource" "example" {
+       triggers = {
+         value = "A example resource that does nothing!"
+       }
+     }
   required_providers {
     digitalocean = {
       source = "digitalocean/digitalocean"
     }
   }
 }
-provider "digitalocean" {
-  token = var.api_token
+# provider "digitalocean" {
+#  token = var.api_token
 
-#DO space details
-  spaces_access_id  = var.access_id
-  spaces_secret_key = var.secret_key
-}
+# }
 
 resource "digitalocean_droplet" "talentgraphlakefs" {
    count = 1
@@ -38,8 +35,8 @@ resource "digitalocean_droplet" "talentgraphlakefs" {
    image = "ubuntu-20-04-x64"
    size = "s-1vcpu-1gb"
    region = var.region
-   ssh_keys = [
-        var.ssh_fingerprint
-   ]
+#   ssh_keys = [
+#       var.ssh_fingerprint
+#   ]
 }
 
