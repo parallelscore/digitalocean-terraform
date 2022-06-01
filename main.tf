3afb419adb65219655b9b95b9cba556956f188ac
Nnolum Esther <esthernnolum@gmail.com>
2022-06-01 06:17:19 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,20 @@
 terraform {
-  backend "s3" {
-    bucket = "sonalysistfstate"
-    key    = "dev/digitaloceantf.tfstate"
-    region = "us-east-2"
+    required_version = ">= 0.11, < 0.12"
+    backend "s3" {
+    skip_requesting_account_id = true
+    skip_credentials_validation = true
+    skip_get_ec2_platforms = true
+    skip_metadata_api_check = true
+    access_key = "var.access_id"
+    secret_key = "var.secret_key"
+    endpoint = "https://fra1.digitaloceanspaces.com"
+    region = "us-east-1"
+    bucket = "sonalysisspace1" // name of your space
+    key = "sonalysistfstate/terraform.tfstate"
   }
-
-  required_providers {
-    digitalocean = {
-      source = "digitalocean/digitalocean"
-    }
-  }
+}  
+provider "digitalocean" {
+  token = var.api_token
 }
 
 resource "digitalocean_droplet" "talentgraphlakefs" {
